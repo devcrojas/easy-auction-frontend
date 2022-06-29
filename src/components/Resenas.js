1e89ee0dcaa6c7b2ef5e296411def6d4c8474df3
utn213271011 <101885873+utn213271011@users.noreply.github.com>
2022-06-28 21:55:16 -0500

    Se agrego Reseñas

--- src/components/Resenas.js
+++ src/components/Resenas.js
@@ -1,13 +1,148 @@
-import React from 'react'
-import { Image } from 'react-bootstrap'
-import Const from '../images/construccion.png'
+import React, { useState } from "react";
+import { FaStar } from "react-icons/fa";
+import { Button, Row, Col, Card, Form } from "react-bootstrap";
+import Swal from 'sweetalert2/dist/sweetalert2.js'
+import '@sweetalert2/theme-material-ui/material-ui.css'
+import { Container } from "@mui/system";
 
-function Resenas() {
-    return (
-        <div>
-            <Image src={Const} fluid></Image>
-        </div>
-    )
+const colors = {
+    yellow: "#ECFF00",
+    grey: "#a9a9a9"
 }
+const styles = {
+    container: {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center"
+    }
+}
+  
+function Resenas(){
+    const stars = Array(5).fill(0);
+    const [Estrellas, setEstrellas] = useState(0);
+    const [hoverValue, setHoverValue] =useState();
+    //const [api, setApi] = useState();
+    const [comentario, setComentario] = useState("");
+    const [tipo, setTipo] = useState("");
 
-export default Resenas
+
+    const [userName, setData] = useState({name:""});
+
+    const handleInputChange = (event) => {
+        setData({
+            ...userName,
+            [event.target.name]: event.target.value
+        })
+    }
+
+
+    const handleClick = value => {
+        setEstrellas(value)
+        if(value <= 2){
+            setTipo("Pésimo Servicio")
+        }
+        else {
+            setTipo("Excelente servicio")
+        }
+    };
+
+    const handleMouseOver = value => {
+        setHoverValue(value)
+    };
+
+    const handleMouseLeave = ()  => {
+        setHoverValue(undefined)
+    }
+
+    const sendResena = async()  => {
+        const resena = {
+            name: userName.name,
+            comment: comentario,
+            stars: Estrellas,
+            type: tipo
+    }
+
+    const myHeaders = new Headers();
+    myHeaders.append('Content-Type', 'application/json');
+    let resp = await fetch('http://localhost:8080/reviews',
+    {
+        method: 'POST',
+        headers: myHeaders,
+        body: JSON.stringify(resena)
+    })
+    if(resp){
+        Swal.fire({
+            icon: 'success',
+            title: '¡Tu Comentario se envió con éxito!',
+        })
+        .then((result) => {
+            /* Read more about isConfirmed, isDenied below */
+            if (result.isConfirmed) {
+              
+              setEstrellas(0);  
+              setComentario("");
+              setData({name:""});
+            }
+        })
+    }
+    else {
+        Swal.fire({
+            icon: 'error',
+            title: '¡Ah ocurrido un error inesperado!',
+        })
+    }
+}
+return (
+    <Container fluid>
+        <Row className="d-flex justify-content-center align-items-center">
+            <Card style={{ width: '50%', padding: "0"}} className="m-3">
+                <Card.Header className='text-center bg-dark text-white' style={{width: "100%"}}>Nos importa tu comentario</Card.Header>
+                <Card.Body>
+                    <Form className='text-center'>
+                        <Form.Group className="mb-3" controlId="user">
+                            <Form.Label>Nombre de Usuario</Form.Label>
+                            <Form.Control value={userName.name} name="name" type="text" placeholder="Nombre" onChange={handleInputChange} />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="stars">
+                        <Row>
+                            <Form className='text-center'>
+                                <div style={styles.stars}>
+                                    {stars.map((_, index) => {
+                                        return (
+                                            <FaStar
+                                            key={index}
+                                            size={30}
+                                            style={{
+                                                marginRight: 10,
+                                                cursor: "pointer"
+                                            }}
+                                            color={(hoverValue || Estrellas) > index ? colors.yellow : colors.grey}
+                                            onClick={() => handleClick(index + 1)}
+                                            onMouseOver={() => handleMouseOver(index + 1)}
+                                            onMouseLeave={handleMouseLeave}
+                                            totalStars={stars}
+                                            />
+                                        )
+                                    })}
+                                </div>
+                            </Form>
+                        </Row>
+                        <Row>
+                            <Form.Group className="mb-3" controlId="comentario">
+                                <Form.Label>¿Qué te parecio la subasta?</Form.Label>
+                                <Form.Control as="textarea" rows="3" value={comentario} onChange={(event) => setComentario(event.target.value)} />
+                            </Form.Group>
+                        </Row>
+                        </Form.Group>
+                        <Button variant="primary" type="button" className='btn btn-success text-center' onClick={sendResena}>
+                                    Enviar
+                        </Button>
+                    </Form>
+                </Card.Body>
+            </Card>
+        </Row>
+    </Container>
+);
+};
+
+export default Resenas;
