8639af554d19214aa0fd3d9b12a1c16f65194184
VianeySandoval <107520981+VianeySandoval@users.noreply.github.com>
2022-07-10 23:44:26 -0500

    Version con el nuevo componente

--- src/components/ProductsCards.js
+++ src/components/ProductsCards.js
@@ -1,8 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import { Col, Container, Modal, Row } from 'react-bootstrap';
-import { Card, CardContent, CardMedia, Typography, CardActionArea, ListItem, CardHeader, Avatar } from '@mui/material'
+import { Card, Button, TextField, CardContent, CardMedia, Typography, CardActionArea, ListItem, CardHeader, Avatar } from '@mui/material'
 import 'react-slideshow-image/dist/styles.css'
 import { Zoom } from 'react-slideshow-image';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
+import CategoryIcon from '@mui/icons-material/Category';
+import SquareFootIcon from '@mui/icons-material/SquareFoot';
+import NewReleasesIcon from '@mui/icons-material/NewReleases';
+import CarpenterIcon from '@mui/icons-material/Carpenter';
+import AssignmentTurnedInIcon from '@mui/icons-material/AssignmentTurnedIn';
+import PaidIcon from '@mui/icons-material/Paid';
+import IconButton from '@mui/material/IconButton';
+import SendIcon from '@mui/icons-material/Send';
 
 function ProductsCards() {
 
@@ -54,80 +64,89 @@ function ProductsCards() {
         let card = apis.map((producto) => {
             let initialDate = new Date(producto.auctionDate.initialD).toLocaleDateString();
             let finalDate = new Date(producto.auctionDate.final).toLocaleDateString() + ' ' + new Date(producto.auctionDate.final).toLocaleTimeString();
-            let imag = (producto.file) ? producto.file.filePath : 'uploads\\sin.jpg' 
+            let imag = (producto.file) ? producto.file.filePath : '\\sin.jpg'
             return (
                 <Col sx={12} lg={6} key={producto._id} className='mb-5'>
-                <Card sx={{ width:'100%',height:'100%', borderRadius: 5,  marginBottom: 10 }} elevation={10} key={producto._id}>
-                    <CardActionArea onClick={ () => {
-                        setDetalles(apis.find(product => product._id === producto._id));
-                        handleShow();
-                    }}>
-                        <CardHeader
-                            avatar={
-                                <Avatar src={producto.sellerData.file.filePath} />
-                            }
-                            title={producto.sellerData.firstName + " " + producto.sellerData.lastName}
-                            subheader={producto.sellerData.email} />
-                        <Row className="justify-content-center my-2">
-                            <CardMedia
-                                id={producto._id}
-                                component="img"
-                                image={`http://localhost:8080\\${imag}`}
-                                alt={producto.nameProduct}
-                                sx={{ width: 350,height: 350 }}
+                    <Card sx={{ width: '100%', height: '100%', borderRadius: 5, marginBottom: 10 }} elevation={10} key={producto._id}>
+                        <CardActionArea onClick={() => {
+                            setDetalles(apis.find(product => product._id === producto._id));
+                            handleShow();
+                        }}>
+                            <CardHeader
+                                avatar={
+                                    <Avatar src={producto.sellerData.file.filePath} />
+                                }
+                                title={producto.sellerData.firstName + " " + producto.sellerData.lastName}
+                                subheader={producto.sellerData.email} />
+                            <Row className="justify-content-center my-2">
+                                <CardMedia
+                                    id={producto._id}
+                                    component="img"
+                                    image={`\\${imag}`}
+                                    alt={producto.nameProduct}
+                                    sx={{ width: 350, height: 350 }}
                                 />
+                            </Row>
+                            <CardContent>
+                                <Row className='my-2'>
+                                    <Col >
+                                        <div className='w-100'>
+                                            <div><Typography component="div" variant="h5">{producto.nameProduct}</Typography></div>
+                                            <div><Typography component="div" >{producto.category}  </Typography></div>
+                                        </div>
+                                    </Col>
+                                </Row>
+                                <Row className='my-2'>
+                                    <Col >
+                                        <div className='w-100'>
+                                            <div><Typography component="div">Precio inicial: </Typography></div>
+                                            <div><Typography component="div"><em><b>$</b></em> {producto.price.initialP} </Typography></div>
+                                        </div>
+                                    </Col>
+                                    <Col >
+                                        <div className='w-100'>
+                                            <div><Typography component="div">Fecha inicio: </Typography></div>
+                                            <div><Typography component="div">{initialDate} </Typography></div>
+                                        </div>
+                                    </Col>
+                                </Row>
+                                <Row className='my-2'>
+                                    <Col>
+                                        <div className='w-100'>
+                                            <div><Typography component="div">Comprar ahora: </Typography></div>
+                                            <div><Typography component="div"><em><b>$</b></em>  {producto.price.buyNow} </Typography></div>
+                                        </div>
+                                    </Col>
+                                    <Col>
+                                        <div className='w-100'>
+                                            <div><Typography component="div">Fecha fin: </Typography></div>
+                                            <div><Typography component="div">{finalDate} </Typography></div>
+                                        </div>
+                                    </Col>
+                                </Row>
+                                <Row className='my-5'>
+                                    <Col>
+                                        <ListItem>
+                                            <Typography component="div" color='success'>Ofertado:<em><b>$</b></em></Typography>
+                                            <Typography component="div" className="text-success">{producto.price.offered} </Typography>
+                                        </ListItem>
+                                    </Col>
+                                </Row>
+                            </CardContent>
+                        </CardActionArea>
+                        <Row>
+                            <Col className="text-center" md={7}>
+                                <TextField key={producto._id} size="small" label="Ofertar" type="number"  InputProps={{ inputProps: { min: 0  }}}  sx={{ borderRadius:15 }}/>
+                                <IconButton aria-label="send">
+                                    <SendIcon color="primary" />
+                                </IconButton>
+                            </Col>
+                            <Col className="text-center" md={5}>
+                                <Button variant="contained" size="large" color="success" >Comprar ahora</Button>
+                            </Col>
                         </Row>
-                        <CardContent>
-                            <Row className='my-2'>
-                                <Col >
-                                    <div className='w-100'>
-                                        <div><Typography component="div" variant="h5">{producto.nameProduct}</Typography></div>
-                                        <div><Typography component="div" >{producto.category}  </Typography></div>
-                                    </div>
-                                </Col>
-                            </Row>
-
-                            <Row className='my-2'>
-                                <Col >
-                                    <div className='w-100'>
-                                        <div><Typography component="div">Precio inicial: </Typography></div>
-                                        <div><Typography component="div"><em><b>$</b></em> {producto.price.initialP} </Typography></div>
-                                    </div>
-                                </Col>
-                                <Col >
-                                    <div className='w-100'>
-                                        <div><Typography component="div">Fecha inicio: </Typography></div>
-                                        <div><Typography component="div">{initialDate} </Typography></div>
-                                    </div>
-                                </Col>
-                            </Row>
-                            <Row className='my-2'>
-                                <Col>
-                                    <div className='w-100'>
-                                        <div><Typography component="div">Comprar ahora: </Typography></div>
-                                        <div><Typography component="div"><em><b>$</b></em>  {producto.price.buyNow} </Typography></div>
-                                    </div>
-                                </Col>
-                                <Col>
-                                    <div className='w-100'>
-                                        <div><Typography component="div">Fecha fin: </Typography></div>
-                                        <div><Typography component="div">{finalDate} </Typography></div>
-                                    </div>
-                                </Col>
-                            </Row>
-                            <Row className='my-5'>
-                                <Col></Col>
-                                <Col>
-                                    <ListItem>
-                                        <Typography component="div" variant='h5' color='success'>Ofertado:<em><b>$</b></em></Typography>
-                                        <Typography component="div" variant='h5' className="text-success">{producto.price.offered} </Typography>
-                                    </ListItem>
-                                </Col>
-                            </Row>
-                        </CardContent>
-                    </CardActionArea>
-                </Card>
-               </Col>
+                    </Card>
+                </Col>
             )
         });
         return card
@@ -137,9 +156,9 @@ function ProductsCards() {
         let initialDate = new Date(detalle.auctionDate.initialD).toLocaleDateString();
         let finalDate = new Date(detalle.auctionDate.final).toLocaleDateString() + ' ' + new Date(detalle.auctionDate.final).toLocaleTimeString();
         return (
-            <Modal show={show} size="xl"  centered onHide={handleClose} >
+            <Modal show={show} size="xl" centered onHide={handleClose} >
                 <Modal.Header closeButton>
-                 <h1> {detalle.nameProduct}</h1>
+                    <h1> {detalle.nameProduct}</h1>
                 </Modal.Header>
                 <Modal.Body>
                     <Row>
@@ -147,23 +166,23 @@ function ProductsCards() {
                             <div className="slide-container" sx={{ width: '75%', height: 'auto' }}>
                                 <>
                                     {
-                                        showSlider(detalle.files,detalle.file)
+                                        showSlider(detalle.files, detalle.file)
                                     }
                                 </>
                             </div>
                         </Col>
                         <Col md={6} className="fuente">
-                            <p>Categoria: {detalle.category}</p>
-                            <p>Material: {detalle.description.material}</p>
-                            <p>Marca: {detalle.description.marca}</p>
-                            <p>Dimensiones: {detalle.description.dimensions}</p>
-                            <p>Condicion: {detalle.description.actualCondition}</p>
-                            <p>Observaciones: {detalle.description.observations}</p>
-                            <p>Precio inicial:${detalle.price.initialP}</p>
-                            <p>Comprar ahora:${detalle.price.buyNow}</p>
-                            <p>Precio ofertado:${detalle.price.offered}</p>
-                            <p>Inicio subasta:{initialDate}</p>
-                            <p>Fin de la subasta: {finalDate}</p>
+                            <p><CategoryIcon color="primary" /> Categoria: {detalle.category}</p>
+                            <p><CarpenterIcon color="primary" /> Material: {detalle.description.material}</p>
+                            <p><AssignmentTurnedInIcon color="primary" /> Marca: {detalle.description.marca}</p>
+                            <p><SquareFootIcon color="primary" /> Dimensiones: {detalle.description.dimensions}</p>
+                            <p><NewReleasesIcon color="primary" /> Condicion: {detalle.description.actualCondition}</p>
+                            <p><RemoveRedEyeIcon color="primary" /> Observaciones: {detalle.description.observations}</p>
+                            <p><PaidIcon color="primary" /> Precio inicial:<em><b>$</b></em>{detalle.price.initialP}</p>
+                            <p><PaidIcon color="primary" /> Comprar ahora:<em><b>$</b></em>{detalle.price.buyNow}</p>
+                            <p><PaidIcon color="primary" /> Precio ofertado:<em><b>$</b></em>{detalle.price.offered}</p>
+                            <p><CalendarMonthIcon color="primary" /> Inicio subasta:{initialDate}</p>
+                            <p><CalendarMonthIcon color="primary" />  Fin de la subasta: {finalDate}</p>
                         </Col>
                     </Row>
                 </Modal.Body>
@@ -171,23 +190,24 @@ function ProductsCards() {
         )
     }
 
-    function showSlider(detImages,principal) {
+    function showSlider(detImages, principal) {
 
         if (detImages.length >= 1) {
             return (
                 <Zoom scale={0.4}>
-                    <img  style={{ width: "100%", height: 350, border: 5 }} src={`http://localhost:8080\\${principal.filePath}`} alt={principal.nameProduct} />
+                    <img style={{ width: "100%", height: 350, border: 5 }} src={`\\${principal.filePath}`} alt={principal.nameProduct} />
                     {
                         detImages.map(((im, index) => {
-                            return <img key={index} style={{ width: "100%", height: 350, border: 5 }} src={`http://localhost:8080\\${im.filePath}`} alt={index} />
+                            return <img key={index} style={{ width: "100%", height: 350, border: 5 }} src={`\\${im.filePath}`} alt={principal.nameProduct} />
                         }))
                     }
                 </Zoom>
             );
-        }else{
-            return <img  style={{ width: "100%", height: 350, border: 5 }} src={`http://localhost:8080\\${principal.filePath}`} alt={principal.nameProduct} />
+        } else {
+            return <img style={{ width: "100%", height: 350, border: 5 }} src={`\\${principal.filePath}`} alt={principal.nameProduct} />
         }
     }
+
 
     return (
         <>
