4d62963279713751bdd3d596b0d6924c18da2f0d
AlanJair <82456954+AlanJair@users.noreply.github.com>
2022-07-20 07:07:54 +0200

    Index actualizado

--- src/index.js
+++ src/index.js
@@ -4,11 +4,16 @@ import './index.css';
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import Welcome from './components/Welcome';
 import Products from './components/Products';
-import Reviews from './components/Reviews';
-import Register from './components/Register';
-import Sellers from './components/Sellers';
 import serviceAuth from './services/auth.service'
 import Profile from './components/Profile';
+import Admin from './components/Admin/Home';
+import ResetPassword from './components/ResetPassword';
+import Points from './components/Points/Points';
+import Reviews from './components/Reviews';
+import CreateProduct from './components/CreateProduct';
+import { Home } from '@mui/icons-material';
+import ProductsAdmin from './components/Admin/ProductsAdmin';
+import MyProducts from './components/MyProducts';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 const ProtectedRoute = ({ children }) => {
@@ -19,10 +24,21 @@ const ProtectedRoute = ({ children }) => {
   return children;
 };
 
+const ProtectedRouteAdmin = ({ children }) => {
+  var user = serviceAuth.getCurrentUser();
+  if (!user) {
+    return <Navigate to="/" replace />;
+  }else if(user.isAdmin){
+    return children;
+  }else{
+    return <Navigate to="/productos" replace/>;
+  }
+};
+
 const ProtectedRouteLogin = ({ children }) => {
   var user = serviceAuth.getCurrentUser();
   if (user) {
-    return <Navigate to="/producto" replace />;
+    return <Navigate to="/productos" replace />;
   }
   return children;
 };
@@ -31,13 +47,17 @@ root.render(
   <Router>
     <Routes>
       <Route exact path="/" element={ <ProtectedRouteLogin >  <Welcome /> </ProtectedRouteLogin>}></Route>
-      <Route exact path='/producto' element={ <ProtectedRoute >  <Home /> </ProtectedRoute> }></Route>
-      <Route exact path="/register" element={<Register />}></Route>
-      <Route exact path="/products" element={<Products />}></Route>
-      <Route exact path="/reviews" element={<Reviews />}></Route>
-      <Route exact path="/sellers" element={<Sellers />}></Route>
-      <Route exact path="/perfil" element={ <ProtectedRoute >  <Profile  /> </ProtectedRoute>}></Route>
+      <Route exact path='/ejemploDeIntegracion' element={ <ProtectedRoute >  <Home /> </ProtectedRoute> }></Route>
+      <Route exact path="/profile" element={ <ProtectedRoute >  <Profile /> </ProtectedRoute>}></Route>
+      <Route exact path="/resetPassword/:jwtoken" element={  <ResetPassword /> }></Route>
+      <Route exact path="/productos" element={ <ProtectedRoute > <Products /> </ProtectedRoute>}></Route>
+      <Route exact path="/resenas" element={<ProtectedRoute> <Reviews /> </ProtectedRoute>}></Route>
+      <Route exact path="/buys/points" element={<ProtectedRoute> <Points /> </ProtectedRoute>}></Route>
+      <Route exact path="/createProducts" element={<ProtectedRoute> <CreateProduct /> </ProtectedRoute>}></Route>
+      <Route exact path="/updateProduct/:productId" element={<ProtectedRoute> <CreateProduct /> </ProtectedRoute>}></Route>
+      <Route exact path="/misproductos" element={<ProtectedRoute> <MyProducts /> </ProtectedRoute>}></Route>
+      <Route exact path="/admin" element={<ProtectedRouteAdmin> <Admin /> </ProtectedRouteAdmin>}></Route>
+      <Route exact path="/admin/productos" element={ <ProtectedRouteAdmin > <ProductsAdmin /> </ProtectedRouteAdmin>}></Route>
     </Routes>
   </Router>
 );
-
