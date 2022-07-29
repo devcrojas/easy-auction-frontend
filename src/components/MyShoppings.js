f98788f77267331da2ddadbba333342818905e78
Cesar Rojas <utn213271020@utn.edu.mx>
2022-07-28 23:14:14 -0500

    Se agregan cambios para que funcione modulo de Alan

--- src/components/MyShoppings.js
+++ src/components/MyShoppings.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, Col, Modal, Row } from 'react-bootstrap';
 import { Zoom } from 'react-slideshow-image';
 import NavBarMenu from './NavBarMenu';
@@ -7,10 +7,22 @@ import ProductsList from './ProductsList';
 import profile from './Profile';
 import 'react-slideshow-image/dist/styles.css'
 import AuthService from '../services/auth.service';
+import PointsService from '../services/points.service'
+
 
 function MyShoppings() {
     const [profile] = useState(AuthService.getCurrentUser().profile);
     const [user] = useState(AuthService.getCurrentUser());
+    const [pointsUser, setPointsUser] = useState();
+
+    useEffect(()=>{
+        const getPoints = async function () {
+            //console.log("Hola");
+            let data = await PointsService.getPointsByUserId(user.id);
+            setPointsUser(data[0])
+          }
+          getPoints();
+    },[])
     return (
         <>
             <NavBarMenu view={"Products"} user={user.profile}></NavBarMenu>
@@ -20,7 +32,7 @@ function MyShoppings() {
                         <MenuLateral view={""} imgProfile={profile.file}></MenuLateral>
                     </Col>
                     <Col xs={9}>
-                        <ProductsList actualView={'myShoppings'}></ProductsList>
+                        <ProductsList setPointsUser={setPointsUser} pointsUser={pointsUser}  actualView={'myShoppings'}></ProductsList>
                     </Col>
                     </Row>
                 </Container>
